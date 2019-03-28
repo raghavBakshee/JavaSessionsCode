38ec2b590b97a07032bbec66afc89fdc1a88db0b
Raghav <Raghav@DESKTOP-EAO9OMP>
2019-03-29 03:28:09 +0530

    Commited test.java

---
+++ src/letsLeetCode/Test.java
@@ -0,0 +1,24 @@
+package letsLeetCode;
+
+class Derived 
+{
+    public void getDetails()
+    {
+        System.out.printf("Derived class ");
+    }
+}
+ 
+public class Test extends Derived
+{
+    public void getDetails()
+    {
+        System.out.printf("Test class ");
+        super.getDetails();
+    }
+    public static void main(String[] args)
+    {
+        Derived obj = new Test();
+        obj.getDetails();
+    }
+}
+///This line reflects Raghav_Master
